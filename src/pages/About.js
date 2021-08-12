cb0d643408555c5f31d4ab6dde7c1c39abccac41
MGedney1 <maurice@gedney.uk>
2021-08-12 20:06:20 +0100

    Added React Router

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <header>
+                <h1>
+                    Job Hunt Blog
+                </h1>
+            </header>
+            <body>
+                <h2>
+                    About
+                </h2>
+            </body>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <header>
+                <h1>
+                    Job Hunt Blog
+                </h1>
+            </header>
+            <body>
+                <h2>
+                    Home
+                </h2>
+            </body>
+        </div>
+    )
+}
+
+export default Home;
